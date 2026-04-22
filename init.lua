4b1c39fdf80e932f19881880fdeaf8f932e37655
Hassan2bit <hassanamiri.ai@gmail.com>
2026-04-22 14:12:54 +0100

    add copilot chat

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim --------------------------------------------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -20,58 +20,112 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Load core modules ---------------------------------------------------------
-require("options")      -- vim.opt settings
-require("keymaps")      -- key mappings
-require("autocmds")     -- autocommands
+require("options") -- vim.opt settings
+require("keymaps") -- key mappings
+require("autocmds") -- autocommands
 
 -- Plugins -------------------------------------------------------------------
 require("lazy").setup({
-  spec = {
-    -- Colorscheme
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    
--- Core plugins
-{ "folke/which-key.nvim", event = "VeryLazy" },  -- <-- ADD THIS
-{ "folke/flash.nvim", event = "VeryLazy" },
-{ "nvim-tree/nvim-web-devicons", lazy = true },
-{ "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
-{ "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
-{ "lewis6991/gitsigns.nvim" },
-{ "goolord/alpha-nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
-    -- Telescope
-    {
-      "nvim-telescope/telescope.nvim",
-      tag = "0.1.6",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-      },
-    },
+	spec = {
+		-- Colorscheme
+		{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+		ui = {
+			timeout = 120, -- Increase timeout to 120 seconds
+		},
+		-- Core plugins
+		{ "folke/which-key.nvim", event = "VeryLazy" },
+		{ "folke/flash.nvim", event = "VeryLazy" },
+		{ "nvim-tree/nvim-web-devicons", lazy = true },
+		{ "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
+		{ "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+		{ "lewis6991/gitsigns.nvim" },
+		{ "goolord/alpha-nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
 
-    -- Discord Presence
-    {
-      "andweeb/presence.nvim",
-      event = "VeryLazy",
-      config = function()
-        require("presence").setup({
-          auto_update = true,
-          neovim_image_text = "Neovim",
-          main_image = "neovim",
-          client_id = "793271441293967371",
-        })
-      end,
-    },
+		-- Telescope
+		{
+			"nvim-telescope/telescope.nvim",
+			tag = "0.1.6",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+				{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			},
+		},
 
-    -- LSP and completion
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
-    { "WhoIsSethDaniel/mason-tool-installer.nvim" },
-    { "neovim/nvim-lspconfig" },
-    { "hrsh7th/nvim-cmp", dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip" } },
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-    { "stevearc/conform.nvim" }, -- formatter
-  },
-  install = { colorscheme = { "catppuccin" } },
+		-- Discord Presence
+		{
+			"andweeb/presence.nvim",
+			event = "VeryLazy",
+			config = function()
+				require("presence").setup({
+					auto_update = true,
+					neovim_image_text = "Neovim",
+					main_image = "neovim",
+					client_id = "793271441293967371",
+				})
+			end,
+		},
+
+		-- LSP and completion
+		{ "williamboman/mason.nvim" },
+		{ "williamboman/mason-lspconfig.nvim" },
+		{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
+		{ "neovim/nvim-lspconfig" },
+		{ "hrsh7th/nvim-cmp", dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip" } },
+		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+		{ "stevearc/conform.nvim" },
+
+		-- Git Management & Diff
+		{
+			"sindrets/diffview.nvim",
+			cmd = { "DiffviewOpen", "DiffviewClose" },
+			keys = {
+				{ "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffView Open" },
+				{ "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
+			},
+		},
+		{
+			"NeogitOrg/neogit",
+			dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
+			config = true,
+			keys = {
+				{ "<leader>gn", "<cmd>Neogit<cr>", desc = "Neogit" },
+			},
+		},
+		-- GitHub Copilot & Chat
+		{
+			"zbirenbaum/copilot.lua",
+			cmd = "Copilot",
+			event = "InsertEnter",
+			config = function()
+				require("copilot").setup({
+					suggestion = {
+						enabled = true,
+						auto_trigger = true,
+						keymap = {
+							accept = "<M-l>",
+							next = "<M-]>",
+							prev = "<M-[>",
+							dismiss = "<C-]>",
+						},
+					},
+					panel = { enabled = false },
+				})
+			end,
+		},
+		{
+			"CopilotC-Nvim/CopilotChat.nvim",
+			dependencies = {
+				{ "zbirenbaum/copilot.lua" },
+				{ "nvim-lua/plenary.nvim" },
+			},
+			build = "make utf8",
+			opts = { debug = false },
+			keys = {
+				{ "<leader>cc", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat - Toggle" },
+			},
+		},
+	},
+	install = { colorscheme = { "catppuccin" } },
 })
 
 -- Load plugin configurations ------------------------------------------------
